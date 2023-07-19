d26c50a1d698bc7b8a28f26bef607b4eb682dd87
daniela <daniela.lucia.villa@hotmail.com>
2023-07-19 20:43:46 -0300

    hice una estructura basica de la tabla, faltan funcion mapeado para cada fila y detalles de botones, etc

--- frontend/src/components/CrudDetail.jsx
+++ frontend/src/components/CrudDetail.jsx
@@ -1,8 +1,38 @@
+import { Container, Table } from "react-bootstrap"
+
 
 function CrudDetail() {
+
+
+
+
     return (
         <>
-
+            <Container>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Materia</th>
+                            <th>Nota Final</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* debo hacer la funcionalidad para que un <td> mapee la materia y el otro td mapee la nota */}
+                        <tr>
+                            <td>Matematicas</td>
+                            <td>8.5</td>
+                        </tr>
+                        <tr>
+                            <td>Historia</td>
+                            <td>7.2</td>
+                        </tr>
+                        <tr>
+                            <td>Ciencias</td>
+                            <td>9.0</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Container>
         </>
     )
 }
