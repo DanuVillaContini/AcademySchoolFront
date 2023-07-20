65f3f81c4f9fdd698ff870441a90a356ab6ff7ce
daniela <daniela.lucia.villa@hotmail.com>
2023-07-20 13:36:45 -0300

    componente tabla detalles de materias e instalacion icon-boostrap. Falta la logica con el back y detalles no vistos aun

--- frontend/src/components/CrudDetail.jsx
+++ frontend/src/components/CrudDetail.jsx
@@ -1,37 +1,65 @@
-import { Container, Table } from "react-bootstrap"
+import { Button, Col, Container, Row, Table } from "react-bootstrap"
+import "../styles/detailStyle.css"
 
 
 function CrudDetail() {
 
-
+    //Esto reemplazar por la logica dle back, para traer las notas del alumno
+    const prueba = [
+        { _id: 1, materia: 'Historia', nota: 7.2 },
+        { _id: 2, materia: 'Matemáticas', nota: 8.5 },
+        { _id: 3, materia: 'Ciencias', nota: 9.0 },
+        { _id: 4, materia: 'Lenguaje', nota: 6.8 },
+        { _id: 5, materia: 'Artes', nota: 8.0 },
+        { _id: 6, materia: 'Geografía', nota: 7.5 },
+        { _id: 7, materia: 'Física', nota: 8.9 },
+        { _id: 8, materia: 'Química', nota: 9.2 },
+        { _id: 9, materia: 'Biología', nota: 8.7 }
+    ];
 
 
     return (
         <>
-            <Container>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Materia</th>
-                            <th>Nota Final</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* debo hacer la funcionalidad para que un <td> mapee la materia y el otro td mapee la nota */}
-                        <tr>
-                            <td>Matematicas</td>
-                            <td>8.5</td>
-                        </tr>
-                        <tr>
-                            <td>Historia</td>
-                            <td>7.2</td>
-                        </tr>
-                        <tr>
-                            <td>Ciencias</td>
-                            <td>9.0</td>
-                        </tr>
-                    </tbody>
-                </Table>
+            <Container >
+                <Row className="align-items-center flex-column custom-container ">
+                    <Col className="d-flex justify-content-center">
+                        <h2>Detalle Notas Finales</h2>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <div className="custom-container-span">
+                            <i className="bi bi-person-bounding-box m-2">
+                                <span className="m-2">Alumno: Fulano Juarez</span>
+                            </i></div>
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Table className="custom-table" striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Materia</th>
+                                <th>Nota Final</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {prueba.map((prueba) => (
+                                <tr key={prueba._id}>
+                                    <td>{prueba.materia}</td>
+                                    <td>{prueba.nota}</td>
+                                    <td className="d-flex justify-content-center"><Button variant='outline-danger' className="m-1">
+                                        <i className="bi bi-trash-fill" ></i>
+                                    </Button>
+                                        <Button variant='outline-success' className="m-1">
+                                            <i className="bi bi-pencil-square"></i>
+                                        </Button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </Row>
+
             </Container>
         </>
     )
