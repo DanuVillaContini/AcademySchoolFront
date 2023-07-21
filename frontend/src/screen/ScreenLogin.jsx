a4f829c267471bebfba6e6b8b614f766e0c236cf
Pablocolombres <pablo010010@gmail.com>
2023-07-21 12:28:20 -0300

    agrego estructura base de login

--- frontend/src/screen/ScreenLogin.jsx
+++ frontend/src/screen/ScreenLogin.jsx
@@ -2,9 +2,28 @@ import "../styles/loginStyle.css"
 
 function ScreenLogin() {
     return (
-        <div>
-            <h1>LOGIN</h1>
-        </div>
+        <div className="login-container">
+      <div className="background-image" />
+      <div className="login-content">
+        <div className="logo-placeholder" />
+        <h2>Bienvenido</h2>
+        <form>
+          <label htmlFor="username">Usuario</label>
+          <input type="text" id="username" name="username" />
+
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" id="password" name="password" />
+
+          <div className="remember-me">
+            <input type="checkbox" id="remember" name="remember" />
+            <label htmlFor="remember">Recordar usuario</label>
+          </div>
+
+          <button type="submit">Iniciar sesión</button>
+        </form>
+        <p className="register-link">¿Aún no tienes cuenta? Regístrate aquí.</p>
+      </div>
+    </div>
     )
 }
 
