48e3390c9fa20835d52d196bdd55e275b03ca27a
daniela <daniela.lucia.villa@hotmail.com>
2023-07-23 02:57:54 -0300

    responsive grid loyout, hice pruebas con navbar y sidebar falses

--- frontend/src/components/GridLoyout.jsx
+++ frontend/src/components/GridLoyout.jsx
@@ -1,14 +1,19 @@
 import "../styles/gridLoyout.css"
+// import NavBarCustom from "./NavBarCustom"
+// import SideBarLateral from "./SideBarLateral"
 
-function GridLoyout({ props_navbar, props_sidebar, props_content, props_footer }) {
+function GridLoyout({  props_content, props_footer }) {
+
+    // const userName = "Nombre User"
+
     return (
         <div className="grid-container">
             <div className="navbar">
-                {props_navbar}
+                {/* <NavBarCustom name_user={userName} /> */}
             </div>
 
             <div className="sidebar">
-                {props_sidebar}
+                {/* <SideBarLateral/> */}
             </div>
 
             <div className="content">
---
+++ frontend/src/components/SideBarLateral.jsx
@@ -0,0 +1,12 @@
+
+
+function SideBarLateral() {
+
+
+    return(
+
+    )
+
+}
+
+export default SideBarLateral
