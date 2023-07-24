0cb068bc93116a8fb633c0157faf5b85969c1289
Pablocolombres <pablo010010@gmail.com>
2023-07-23 21:08:08 -0300

    agrego logo

--- frontend/src/screen/ScreenLogin.jsx
+++ frontend/src/screen/ScreenLogin.jsx
@@ -1,22 +1,25 @@
-import "../styles/loginStyle.css"
+import logo from '../assets/logo.png';
+import "../styles/loginStyle.css";
 
 function ScreenLogin() {
-    return (
-        <div className="login-container">
+  return (
+    <div className="login-container">
       <div className="background-image" />
       <div className="login-content">
-        <div className="logo-placeholder"/>
+      <img src={logo} alt="Logo de la página" className="logo" />
         <h2>Bienvenido</h2>
         <form>
           <label htmlFor="username">Usuario</label>
-          <input type="text" id="username" name="username" maxLength="15"/>
+          <input type="text" id="username" name="username" maxLength="15" />
 
           <label htmlFor="password">Contraseña</label>
           <input type="password" id="password" name="password" maxLength="15" />
 
           <div className="remember-me">
             <input type="checkbox" id="remember" name="remember" />
-            <label className="remember-label" htmlFor="remember">Recordar usuario</label>
+            <label className="remember-label" htmlFor="remember">
+              Recordar usuario
+            </label>
           </div>
 
           <button type="submit">Iniciar sesión</button>
@@ -24,7 +27,7 @@ function ScreenLogin() {
         <p className="register-link">¿Aún no tienes cuenta? Regístrate aquí.</p>
       </div>
     </div>
-    )
+  );
 }
 
-export default ScreenLogin
+export default ScreenLogin;
