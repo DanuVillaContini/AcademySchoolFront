9ff5509e4f119feaca31b5fe17654ec64b9fbdcf
Pablocolombres <pablo010010@gmail.com>
2023-07-29 17:31:34 -0300

    agrego enrrutamiento para el form institucion en app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import MenuRouter from "./routes/MenuRouter";
 import ScreenLogin from "./screen/ScreenLogin";
+import InstitutionForm from './InstitutionForm';
 
 function App() {
 
