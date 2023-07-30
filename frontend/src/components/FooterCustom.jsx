f1ca3f7edfd03786cbf4b41811fe54e6471492be
MauricioPerezIsa <mauricioexequielperezisa@gmail.com>
2023-07-29 22:54:08 -0300

    creacion de componente Footer

--- frontend/src/components/FooterCustom.jsx
+++ frontend/src/components/FooterCustom.jsx
@@ -1,10 +1,16 @@
+import React from "react"
+import '../styles/footerStyle.css'
+
 
 function FooterCustom() {
     return (
-        <>
-
-        </>
-    )
-}
-
+        <div className="container-fluid text-center py-4 mt-auto footerCustom">
+        <div className="d-flex align-items-center justify-content-center">
+            <img src="" alt="" />
+          <p className="m-0 h5">AcademySchool</p>
+        </div>
+      </div>
+        
+        )
+    }
 export default FooterCustom
