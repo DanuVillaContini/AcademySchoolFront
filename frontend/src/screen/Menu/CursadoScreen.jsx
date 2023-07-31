72c1b6074fab4ab4888dd1dbecc8f16c51afba54
daniela <daniela.lucia.villa@hotmail.com>
2023-07-31 17:52:45 -0300

    merge loyout

--- frontend/src/screen/Menu/CursadoScreen.jsx
+++ frontend/src/screen/Menu/CursadoScreen.jsx
@@ -1,15 +1,12 @@
-import GridLoyout from "../../components/GridLoyout"
+
 
 function CursadoScreen() {
 
-    const content = <p>CRUD DETALLES CURSADO</p>;
-    const footerContent = <span> FOOTER</span>;
+
 
     return (
         <>
-            <GridLoyout
-                props_content={content}
-                props_footer={footerContent} />
+
         </>
     )
 }
