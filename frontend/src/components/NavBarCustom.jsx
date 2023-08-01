2a3e8a3fdff40bc6b4141ae2b349d42626633f35
daniela <daniela.lucia.villa@hotmail.com>
2023-08-01 17:46:31 -0300

    Loyout, navbar, sidebar Arreglados

--- frontend/src/components/NavBarCustom.jsx
+++ frontend/src/components/NavBarCustom.jsx
@@ -1,16 +1,32 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { useState } from "react";
+import { Navbar, Container, Nav, Accordion } from "react-bootstrap";
 import Logo from "../assets/logo_recortado.png";
 import styles from "../styles/navbarStyle.module.css";
-
-
-
+import { Link } from "react-router-dom";
 
 function NavBarCustom({ name_user }) {
+
+    const [isAccordionOpen, setIsAccordionOpen] = useState(false); //para controlar que no se abra el acordeon al recargar la page
+
+
+
+
+
+    //para controlar que no se abra el acordeon al recargar la page
+    const toggleAccordion = () => {
+        setIsAccordionOpen(!isAccordionOpen);
+    };
+
+
+
     return (
-        //sticky="top"
-        <Navbar sticky="top"  className={`justify-content-center ${styles['wrapper-navbar']}`} expand="md">
+        <Navbar
+            sticky="top"
+            className={`justify-content-center ${styles["wrapper-navbar"]}`}
+            expand="md"
+        >
             <Container className={styles["custom-navbar"]}>
-                <div className={`d-flex align-items-center ${styles['custom-logo']}`} >
+                <div className={`d-flex align-items-center ${styles["custom-logo"]}`}>
                     <img
                         alt=""
                         src={Logo}
@@ -18,24 +34,36 @@ function NavBarCustom({ name_user }) {
                         height="85"
                         className="d-inline-block align-top "
                     />
-                    <Navbar.Brand className="ms-2 ">Academy School</Navbar.Brand>
+                    <Navbar.Brand className="ms-2 text-light ">ACADEMY SCHOOL</Navbar.Brand>
                 </div>
-                <Navbar.Toggle aria-controls="basic-navbar-nav " />
-                <Navbar.Collapse id="basic-navbar-nav" className="d-lg-flex justify-content-lg-end ">
-                    <Nav className="ms-md-auto ">
-                        <NavDropdown
-                            menuVariant="dark"
-                            className={`fw-bold font-monospace ${styles['custom-navdropdown']}`}
-                            title={
-                                <>
-                                    <i className="bi bi-person-lines-fill m-1 text-dark"></i>
+                <Navbar.Collapse id="basic-navbar-nav" className="d-lg-flex justify-content-lg-end">
+                    <Nav className="ms-md-auto " >
+                        <Accordion defaultActiveKey={isAccordionOpen ? "0" : null} >
+                            <Accordion.Item eventKey="0" className={styles["custom-accordion"]}>
+
+                                <Accordion.Header onClick={toggleAccordion} >
+                                    <i className="bi bi-person-lines-fill m-2 text-dark"></i>
                                     {name_user}
-                                </>
-                            }
-                            id="basic-nav-dropdown"
-                        >
-                            <NavDropdown.Item className=" text-center fs-5 font-monospace" href="#">Logout</NavDropdown.Item>
-                        </NavDropdown>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    <Link to="/menu/" className={` text-decoration-none d-lg-none ${styles["custom-nav-link"]}`}>
+                                        <span className="text-light text-center fs-5 font-monospace" >
+                                            Personal
+                                        </span>
+                                    </Link>
+                                    <Link to="/menu/alumnos" className={` text-decoration-none d-lg-none ${styles["custom-nav-link"]}`}>
+                                        <span className="text-light text-center fs-5 font-monospace" >
+                                            Alumnos
+                                        </span>
+                                    </Link>
+                                    <Link to="#" className={` text-decoration-none ${styles["custom-nav-link"]}`}>
+                                        <span className="text-light text-center fs-5 font-monospace" >
+                                            Logout
+                                        </span>
+                                    </Link>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- frontend/src/components/SideBarLateral.jsx
+++ frontend/src/components/SideBarLateral.jsx
@@ -1,47 +1,26 @@
 import { Link } from "react-router-dom";
-import { Nav, NavDropdown } from "react-bootstrap";
-import styles from  '../styles/sidebarStyle.module.css';
+import styles from '../styles/sidebarStyle.module.css';
 
 function SideBarLateral() {
     return (
         <div className={styles['container-wrapper']}>
 
-            
+
             <div className={styles['sidebar-conteiner']}>
-                {/* Utilizamos NavDropdown para el desplegable en pantallas pequeñas */}
-                <NavDropdown title="Menú" id="basic-nav-dropdown" className="d-lg-none text-dark fw-bold">
-                    <div className={`p-1 ${styles['wrapper-link custom-link']}`}>
-                        <Link to="/menu/" className="text-decoration-none">
-                            <Nav.Link as="span" className="text-light fw-normal">
-                                Personal Institución
-                            </Nav.Link>
-                        </Link>
-                    </div>
-                    <hr className="bg-light"/>
-                    <div className={`p-1 ${styles['wrapper-link custom-link']}`}>
-                        <Link to="/menu/alumnos" className="text-decoration-none">
-                            <Nav.Link as="span" className="text-light fw-normal">
-                                Alumnos Institución
-                            </Nav.Link>
-                        </Link>
-                    </div>
-                </NavDropdown>
 
-
-                {/* En pantallas grandes, mostramos los links */}
-                <div className="d-none d-lg-block">
-                    <div className={`p-1 ${styles['wrapper-link custom-link']}`}>
-                        <Link to="/menu/" className="text-decoration-none">
-                            <Nav.Link as="span" className="text-white">
-                                Personal Institución
-                            </Nav.Link>
+                <div className={`d-none d-lg-block text-center ${styles['']}`}>
+                    <div className={`p-2 m-3 ${styles['wrapper-link custom-link']}`}>
+                        <Link to="/menu/" className={`text-decoration-none ${styles['links-custom']}`}>
+                            <span className="text-light text-center font-monospace fs-5 p-2" >
+                                Personal
+                            </span>
                         </Link>
                     </div>
-                    <div className={`p-1 ${styles['wrapper-link custom-link']}`}>
-                        <Link to="/menu/alumnos" className="text-decoration-none">
-                            <Nav.Link as="span" className="text-white">
-                                Alumnos Institución
-                            </Nav.Link>
+                    <div className={`p-2 m-3 ${styles['wrapper-link custom-link']}`}>
+                        <Link to="/menu/alumnos" className={`text-decoration-none ${styles['links-custom']}`}>
+                            <span className="text-light text-center  font-monospace fs-5 p-2" >
+                                Alumnos 
+                            </span>
                         </Link>
                     </div>
                 </div>
