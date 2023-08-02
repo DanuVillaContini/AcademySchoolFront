ebdc8c8aae93d73e8db877db405c9ac326d78118
MauricioPerezIsa <mauricioexequielperezisa@gmail.com>
2023-08-02 15:32:28 -0300

    Modificacion en hoja de estilos de Footer

--- frontend/src/components/FooterCustom.jsx
+++ frontend/src/components/FooterCustom.jsx
@@ -1,9 +1,9 @@
-import '../styles/footerStyle.css'
+import Style from '../styles/footerStyle.module.css'
 
 
 function FooterCustom() {
     return (
-        <div className="container-fluid text-center py-4 mt-auto footerCustom">
+        <div className={`container-fluid text-center py-4 mt-auto ${Style[footerCustom]}`}>
         <div className="d-flex align-items-center justify-content-center">
             <img src="" alt="" />
           <p className="m-0 h5">AcademySchool</p>
