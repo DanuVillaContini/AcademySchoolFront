daa0dfa3556aa25e52d7cff2bd62bfbfc04c2dc9
nicolmarquezok <nicolmarquezok@gmail>
2023-08-02 12:47:19 -0300

    buttons

--- frontend/src/screen/ScreenLogin.jsx
+++ frontend/src/screen/ScreenLogin.jsx
@@ -1,13 +1,23 @@
 import ButtonCustom from "../components/ButtonCustom"
+import ButtonIconCustom from "../components/ButtonIconCustom"
 import "../styles/loginStyle.css"
 
 function ScreenLogin() {
     return (
         <div>
         <h1>Login</h1>
-
+     
         <ButtonCustom nombre="Cancelar" />
         <ButtonCustom nombre="Login" />
+        <ButtonIconCustom icon="bi bi-trash" tooltip={"Delete"} />
+        <ButtonIconCustom icon="bi bi-telephone-fill" tooltip={"Telefono"} />
+        <ButtonIconCustom icon="bi bi-person-add" tooltip={"Agregar"} />
+        <ButtonIconCustom icon="bi bi-mortarboard-fill" tooltip={"Alumno"} />
+
+
+
+
+        
 
         </div>
         
