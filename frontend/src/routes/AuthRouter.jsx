64834d5e4aba6d570b52d3858ac49666ba227203
Pablocolombres <pablo010010@gmail.com>
2023-08-12 12:49:27 -0300

    se corrige AuthRoute

---
+++ frontend/src/routes/AuthRouter.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import { Route, Routes } from "react-router-dom";
+import InstitutionForm from "../screen/InstitutionForm";
+import AlumnosScreen from "../screen/Menu/AlumnosScreen";
+import CursadoScreen from "../screen/Menu/CursadoScreen";
+import PersonalScreen from "../screen/Menu/PersonalScreen";
+
+function AuthRouter({ jwt = undefined, destroyJwt = () => undefined }) {
+    return (
+        <>
+            <Routes>
+                {
+                    // si no inicia sesion muestro mensaje
+                    !jwt
+                        ? <Route path="/*" element={<h1>No inicio sesion aun, nos vemos en disney</h1>} />
+                        : jwt?.datosActualizados === false // si no actualizo los datos de institcion forzamos el ingreso a esa pantalla
+                        ? <Route path="/*" element={<InstitutionForm />} />
+                        : (
+                            <>
+                                {/* Ya tiene acceso a la aplicacion de manera libre */}
+                                <Route path="/" element={<PersonalScreen destroyJwt={destroyJwt} />} />
+                                <Route path="/alumnos" element={<AlumnosScreen destroyJwt={destroyJwt} />} />
+                                <Route path="/detalle-cursado" element={<CursadoScreen destroyJwt={destroyJwt} />} />
+                                <Route path="/institucion" element={<InstitutionForm  />} />
+                            </>
+                        )
+                }
+            </Routes>
+        </>
+    )
+}
+
+AuthRouter.propTypes = {
+    jwt: PropTypes.object,
+    destroyJwt: PropTypes.func
+}
+
+export default AuthRouter
