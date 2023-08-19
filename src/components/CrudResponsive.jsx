c4264b5d785f9132499019b00c9230270ebff935
Pablocolombres <pablo010010@gmail.com>
2023-08-19 12:07:31 -0300

    se incluye formulario de actualizacion

--- src/components/CrudResponsive.jsx
+++ src/components/CrudResponsive.jsx
@@ -137,15 +137,140 @@ function CrudALumnos() {
         <Col xs={12}>
           <ButtonCustom onClick={() => setShowCreateForm(prevState => !prevState)} nameBtt={showCreateForm ? "Cancelar" : "Nuevo Estudiante"} />
           <Form className={` ${Styles["categories__create-form"]}`} style={{ display: showCreateForm ? "block" : "none" }}>
-            {/* ... Contenido del formulario de creación aquí ... */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className="font-monospace text-decoration-none">Nombre</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Nombre"
+                                value={NombreAlumno}
+                                onChange={(e) => {
+                                    const onlyLettersAndSpaces = e.target.value.replace(/[^A-Za-z\s]/g, "");
+                                    setNombreAlumno(onlyLettersAndSpaces);
+                                }}
+                                maxLength={25}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className="font-monospace text-decoration-none">Apellido</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Apellido"
+                                value={ApellidoAlumno}
+                                onChange={(e) => {
+                                    const onlyLettersAndSpaces = e.target.value.replace(/[^A-Za-z\s]/g, "");
+                                    setApellidoAlumno(onlyLettersAndSpaces);
+                                }}
+                                maxLength={25}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className="font-monospace text-decoration-none">N° DNI</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="N° DNI"
+                                value={DNIAlumno}
+                                onChange={(e) => {
+                                    const input = e.target.value
+                                    const onlyNumbers = input.replace(/[^0-9]/g, "");
+                                    setDNIAlumno(onlyNumbers);
+                                }}
+                                maxLength={8}
+                                minLength={7} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className="font-monospace text-decoration-none">Año</Form.Label>
+                            <Form.Control type="number"
+                                placeholder="Año"
+                                value={AnioAlumno}
+                                onChange={(e) => {
+                                    const newValue = parseInt(e.target.value);
+                                    if (!isNaN(newValue) && newValue >= 1 && newValue <= 4) {
+                                        setAnioAlumno(newValue);
+                                    }
+                                }}
+                            />
+                        </Form.Group>
+                        <ButtonCustomRedGreen color="green" nameBtt="Cargar Estudiante" onClick={handleSubmit} disabled={!NombreAlumno || !ApellidoAlumno || !DNIAlumno || !AnioAlumno} />
           </Form>
         </Col>
       </Row>
-      {/* Formulario de actualización */}
       {updateId.length > 0 && showUpdateForm && (
         <Row>
           <Col xs={12}>
-            {/* ... Contenido del formulario de actualización aquí ... */}
+          <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label className="font-monospace text-decoration-none">Nombre</Form.Label>
+                                <Form.Control type="text"
+                                    placeholder="Nombre"
+                                    required
+                                    maxLength={25}
+                                    value={updateNombre}
+                                    onChange={(e) => {
+                                        const onlyLettersAndSpaces = e.target.value.replace(/[^A-Za-z\s]/g, "");
+                                        setupdateNombre(onlyLettersAndSpaces);
+                                    }}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label className="font-monospace text-decoration-none">Apellido</Form.Label>
+                                <Form.Control type="text"
+                                    placeholder="Apellido"
+                                    required
+                                    maxLength={25}
+                                    value={updateApellido}
+                                    onChange={(e) => {
+                                        const onlyLettersAndSpaces = e.target.value.replace(/[^A-Za-z\s]/g, "");
+                                        setupdateApellido(onlyLettersAndSpaces);
+                                    }}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label className="font-monospace text-decoration-none">N° DNI</Form.Label>
+                                <Form.Control type="text"
+                                    placeholder="N° DNI"
+                                    required
+                                    maxLength={8}
+                                    minLength={7}
+                                    value={updateDni}
+                                    onChange={(e) => {
+                                        const input = e.target.value
+                                        const onlyNumbers = input.replace(/[^0-9]/g, "");
+                                        setupdateDni(onlyNumbers);
+                                    }}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label className="font-monospace text-decoration-none">Año</Form.Label>
+                                <Form.Control type="number"
+                                    placeholder="Año"
+                                    required
+                                    maxLength={1}
+                                    value={updateAnio}
+                                    onChange={(e) => {
+                                        const newValue = parseInt(e.target.value);
+                                        if (!isNaN(newValue) && newValue >= 1 && newValue <= 4) {
+                                            setupdateAnio(newValue);
+                                        }
+                                    }}
+                                />
+                            </Form.Group>
+                            <Row>
+                                <Col>
+                                    <ButtonCustomRedGreen color="red" nameBtt="Cancelar" onClick={() => {
+                                        setupdateId("")
+                                        setupdateNombre("")
+                                        setupdateDni("")
+                                        setupdateAnio("")
+                                    }} />
+
+                                </Col>
+                                <Col>
+                                    <ButtonCustomRedGreen
+                                        color="green"
+                                        onClick={handleUpdateAlumnos}
+                                        nameBtt="Cargar Actualizacion"
+                                        disabled={!updateNombre || !updateApellido || !updateAnio || !updateDni}
+                                    />
+                                </Col>
+                            </Row>
+                        </Form>
           </Col>
         </Row>
       )}
